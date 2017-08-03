74198b9bd67a2c484de709dd26d7a77f59eb6534
vabranches <sapo1211>
2017-08-02 22:54:24 -0300

    com tipo de projeto

--- app/src/main/java/br/com/teravalt/toastapp/MainActivity.java
+++ app/src/main/java/br/com/teravalt/toastapp/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v4.media.session.PlaybackStateCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import br.com.teravalt.minhabiblioteca.CustomToast;
+import br.com.teravalt.minhabiblioteca.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +14,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         CustomToast customToast = new CustomToast();
-        customToast.showToast(this, "Mensagem de teste");
-
-
-
-
+        if(BuildConfig.REPORT_CRASH) {
+            customToast.showToast(this, "Resport Crash");
+        }else {
+            customToast.showToast(this, "Mensagem de teste");
+        }
     }
 }
